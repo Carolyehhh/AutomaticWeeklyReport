9176c3614fae4d13d5d655147cf5dead513507cf
Carolyehhh <carolyehhh@gmail.com>
2024-12-12 15:30:53 +0800

    全部整合到main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Modules import extract_data, filter_data, write_to_sheet, GoogleSheetProcessor, authenticate_google_sheets, transpose_data_prdline, transpose_data_lifecycle
 from data_SQLquery_list import Operation_data_list, User_data_list_week, Active_User_week, Active_User_month, APP_Session_week, Reg_week
+from Calculation import Contribution_Calculation
 
 # 認證範圍與 API 金鑰
 scopes = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"] # 認證範圍: Google Sheet, Google Drive
@@ -18,7 +19,27 @@ def create_config(data_list, output_sheet_name, output_gid, input_cell, clear_ce
     'data_list': data_list,
     'clear_cell_range': clear_cell_range
 }
+
+# 為用戶數據總覽生成專屬的配置
+def get_overview_config():
+    return create_config(
+        data_list = Operation_data_list,
+        output_sheet_name = 'raw_data',
+        output_gid = 1388457908,
+        clear_cell_range = 'A2:D100',
+        input_cell = 'A2'
+    )
  
+# 為用戶數據貢獻度生成專屬的配置
+def get_contribution_config():
+    return create_config(
+        data_list = User_data_list_week,
+        output_sheet_name = '用戶數據_raw_data',
+        output_gid = 2062253493,
+        clear_cell_range = 'A3:G100',
+        input_cell = 'A2'
+    )
+
 # 依產品線分類
 def process_data(config, transpose_key=None, mode='prdline'): # 將其預設為 None，允許該參數在不需要時被省略，避免必須提供不必要的值。
     # 撈取資料
@@ -35,16 +56,21 @@ def process_data(config, transpose_key=None, mode='prdline'): # 將其預設為 
     processor = GoogleSheetProcessor(client, config)
     processor.clear_and_update_sheet(processed_data)
 
-# # 依生命週期分類
-# def process_data_life(config):
-#     # 撈取資料 (25 weeks/months)
-#     raw_data = extract_data(config['data_list'])
-#     processed_data = transpose_data_lifecycle(raw_data)
+def process_overview_data(config):
+    processor = GoogleSheetProcessor(client, config)
+    processor.run_all_overview()
 
-#     # 寫入 Google Sheet
-#     processor = GoogleSheetProcessor(client, config)
-#     processor.clear_and_update_sheet(processed_data)
+def process_contribution_data(config):
+    processor = GoogleSheetProcessor(client, config)
+    filtered_data = processor.extract_and_filter_data()
+    cleaned_data = processor.clean_data(filtered_data)
 
+    # 計算貢獻度排名
+    contribution_calculator = Contribution_Calculation(cleaned_data)
+    ranked_data = contribution_calculator.Contrbution_Ranking()
+
+    # 寫入 Google Sheet
+    processor.clear_and_update_sheet(ranked_data)
 
 def main():
     # 所有配置清單
@@ -61,7 +87,13 @@ def main():
     # 依次處理每個配置, list  of dictionaries
     for conf in configs:
         config = create_config(conf["data_list"], conf["output_sheet_name"], conf["output_gid"], conf["input_cell"], conf["clear_cell_range"])
-        process_data(config, conf.get("transpose_key"), conf["mode"])
+        process_data(config, conf.get("transpose_key"), conf["mode"]) # 使用 get 方法則能在鍵缺失時返回 None（或其他指定的預設值），這樣程式不會崩潰。
+
+    # 用戶數據總覽
+    process_overview_data(get_overview_config())
+
+    # 用戶數據貢獻度
+    process_contribution_data(get_contribution_config())
 
 if __name__=="__main__":
     main()
